1f53e38896ee4e4ce51c5a2562e02cb61917a1bd
rndhouse <rndhouse@protonmail.com>
2021-05-20 12:16:58 +0100

    Add separate store function for int_vector.

--- include/io.hpp
+++ include/io.hpp
@@ -1,5 +1,6 @@
 #include <sdsl/io.hpp>
 #include <sdsl/int_vector.hpp>
+#include <sdsl/rrr_vector.hpp>
 #include "common.hpp"
 
 #ifndef SDSL_C_IO_H
@@ -7,14 +8,17 @@
 
 #define IO_ID _id
 #define STRUCTURE sdsl::int_vector<0>
+#define INT_VECTOR_STRUCTURE sdsl::int_vector<0>
 
 extern "C"
 {
     typedef void Structure;
 
     bool FN(store_to_file, IO_ID)(const Structure *const v,
-                                  const char *const file,
-                                  bool write_fixed_as_variable);
+                                  const char *const file);
+    bool FN(store_int_vector_to_file, IO_ID)(const Structure *const v,
+                                             const char *const file,
+                                             bool write_fixed_as_variable);
     bool FN(load_from_file, IO_ID)(Structure *v, const char *const file);
 }
 
--- src/io.cpp
+++ src/io.cpp
@@ -1,10 +1,17 @@
 #include "io.hpp"
 
 bool FN(store_to_file, IO_ID)(const Structure *const v,
-                              const char *const file,
-                              bool write_fixed_as_variable)
+                              const char *const file)
 {
     const auto &vec = *reinterpret_cast<const STRUCTURE *>(v);
+    return sdsl::store_to_file(vec, file);
+}
+
+bool FN(store_int_vector_to_file, IO_ID)(const Structure *const v,
+                                         const char *const file,
+                                         bool write_fixed_as_variable)
+{
+    const auto &vec = *reinterpret_cast<const INT_VECTOR_STRUCTURE *>(v);
     return sdsl::store_to_file(vec, file, write_fixed_as_variable);
 }
 
