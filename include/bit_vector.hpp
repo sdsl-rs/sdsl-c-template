0297581679b514c5b7441a765c86b3cdde3e9754
rndhouse <rndhouse@protonmail.com>
2021-05-26 12:27:07 +0100

    Add missing methods. Re-order to match sdsl-lite.

--- include/bit_vector.hpp
+++ include/bit_vector.hpp
@@ -14,13 +14,23 @@ extern "C"
     BitVector *FN(bit_vector_create, BIT_VECTOR_ID)(size_type size, bit_vector_value_type default_value);
     void FN(bit_vector_destroy, BIT_VECTOR_ID)(BitVector *v);
     BitVector *FN(bit_vector_copy, BIT_VECTOR_ID)(BitVector *v);
+    bool FN(bit_vector_empty, BIT_VECTOR_ID)(BitVector *v);
+
+    void FN(bit_vector_resize, BIT_VECTOR_ID)(BitVector *v, const size_type size);
     size_type FN(bit_vector_size, BIT_VECTOR_ID)(BitVector *v);
+    size_type FN(bit_vector_max_size, BIT_VECTOR_ID)(BitVector *v);
+    size_type FN(bit_vector_bit_size, BIT_VECTOR_ID)(BitVector *v);
+    size_type FN(bit_vector_capacity, BIT_VECTOR_ID)(BitVector *v);
+
+    const uint64_t *FN(bit_vector_data, BIT_VECTOR_ID)(BitVector *v);
+
+    bit_vector_value_type FN(bit_vector_get_int, BIT_VECTOR_ID)(BitVector *v, const size_type index, const uint8_t len);
+    void FN(bit_vector_set_int, BIT_VECTOR_ID)(BitVector *v, const size_type index, const bit_vector_value_type value);
+
     bit_vector_value_type FN(bit_vector_get_element, BIT_VECTOR_ID)(BitVector *v, const size_type index);
     void FN(bit_vector_set_element, BIT_VECTOR_ID)(BitVector *v, const size_type index, const bit_vector_value_type value);
-    bool FN(bit_vector_empty, BIT_VECTOR_ID)(BitVector *v);
-    void FN(bit_vector_resize, BIT_VECTOR_ID)(BitVector *v, const size_type size);
-    size_type FN(bit_vector_capacity, BIT_VECTOR_ID)(BitVector *v);
-    const uint64_t *FN(bit_vector_data, BIT_VECTOR_ID)(BitVector *v);
+
+    void FN(bit_vector_flip, BIT_VECTOR_ID)(BitVector *v);
 }
 
 #endif //SDSL_C_BIT_VECTOR_HPP
--- src/bit_vector.cpp
+++ src/bit_vector.cpp
@@ -21,10 +21,58 @@ BitVector *FN(bit_vector_copy, BIT_VECTOR_ID)(BitVector *v)
         new sdsl::bit_vector(vec));
 }
 
+bool FN(bit_vector_empty, BIT_VECTOR_ID)(BitVector *v)
+{
+    const auto &vec = *reinterpret_cast<sdsl::bit_vector *>(v);
+    return vec.empty();
+}
+
+void FN(bit_vector_resize, BIT_VECTOR_ID)(BitVector *v, const size_type size)
+{
+    auto &vec = *reinterpret_cast<sdsl::bit_vector *>(v);
+    vec.resize(size);
+}
+
 size_type FN(bit_vector_size, BIT_VECTOR_ID)(BitVector *v)
 {
     const auto &vec = *reinterpret_cast<sdsl::bit_vector *>(v);
     return vec.size();
+}
+
+size_type FN(bit_vector_max_size, BIT_VECTOR_ID)(BitVector *v)
+{
+    const auto &vec = *reinterpret_cast<sdsl::bit_vector *>(v);
+    return vec.max_size();
+}
+
+size_type FN(bit_vector_bit_size, BIT_VECTOR_ID)(BitVector *v)
+{
+    const auto &vec = *reinterpret_cast<sdsl::bit_vector *>(v);
+    return vec.bit_size();
+}
+
+size_type FN(bit_vector_capacity, BIT_VECTOR_ID)(BitVector *v)
+{
+    const auto &vec = *reinterpret_cast<sdsl::bit_vector *>(v);
+    return vec.capacity();
+}
+
+const uint64_t *FN(bit_vector_data, BIT_VECTOR_ID)(BitVector *v)
+{
+    const auto &vec = *reinterpret_cast<const sdsl::bit_vector *>(v);
+    return vec.data();
+}
+
+bit_vector_value_type FN(bit_vector_get_int, BIT_VECTOR_ID)(BitVector *v, const size_type index, const uint8_t len)
+{
+    const auto &vec = *reinterpret_cast<sdsl::bit_vector *>(v);
+    return vec.get_int(index, len);
+}
+
+void FN(bit_vector_set_int, BIT_VECTOR_ID)(BitVector *v, const size_type index, const bit_vector_value_type value, const uint8_t len)
+{
+    auto &vec = *reinterpret_cast<sdsl::bit_vector *>(v);
+    vec.set_int(index, value, len);
 }
 
 bit_vector_value_type FN(bit_vector_get_element, BIT_VECTOR_ID)(BitVector *v, const size_type index)
@@ -39,26 +87,8 @@ void FN(bit_vector_set_element, BIT_VECTOR_ID)(BitVector *v, const size_type ind
     vec[index] = value;
 }
 
-bool FN(bit_vector_empty, BIT_VECTOR_ID)(BitVector *v)
-{
-    const auto &vec = *reinterpret_cast<sdsl::bit_vector *>(v);
-    return vec.empty();
-}
-
-void FN(bit_vector_resize, BIT_VECTOR_ID)(BitVector *v, const size_type size)
+void FN(bit_vector_flip, BIT_VECTOR_ID)(BitVector *v)
 {
     auto &vec = *reinterpret_cast<sdsl::bit_vector *>(v);
-    vec.resize(size);
-}
-
-size_type FN(bit_vector_capacity, BIT_VECTOR_ID)(BitVector *v)
-{
-    const auto &vec = *reinterpret_cast<sdsl::bit_vector *>(v);
-    return vec.capacity();
-}
-
-const uint64_t *FN(bit_vector_data, BIT_VECTOR_ID)(BitVector *v)
-{
-    const auto &vec = *reinterpret_cast<const sdsl::bit_vector *>(v);
-    return vec.data();
+    vec.flip();
 }
