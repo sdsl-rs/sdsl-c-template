ebd11301cc1e2c1e71ec959e2748673cfa58e645
rndhouse <rndhouse@protonmail.com>
2022-01-22 23:05:42 +0000

    Move release archive build to build directory.

--- build_release.sh
+++ build_release.sh
@@ -1,11 +1,16 @@
-mkdir sdsl-c
+mkdir -p build/release_archive
+(cd build/release_archive && mkdir sdsl-c)
 
 shopt -s extglob
 shopt -s dotglob
-cp -r !(sdsl-c) sdsl-c/
+cp -r !(build) build/release_archive/sdsl-c/
+
+cd build/release_archive
 
 (cd sdsl-c && git clean -fdx && git submodule update --init --recursive --depth=1)
 (cd sdsl-c/external/sdsl-lite && git clean -fdx)
+
+(cd sdsl-c && rm -rf .git)
 7z a -tzip sdsl-c-template.zip sdsl-c
 
 rm -rf sdsl-c
